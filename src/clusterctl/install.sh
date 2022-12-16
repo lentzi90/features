bd9393303c25e8ed252338f5beb518a1ce1bbe49
Lennart Jern <lennart.jern@gmail.com>
2022-12-16 20:02:03 +0000

    Install wget if missing
    Ensure bash_completion.d folder exists

--- src/clusterctl/install.sh
+++ src/clusterctl/install.sh
@@ -17,6 +17,17 @@ case $architecture in
     *) echo "(!) Architecture $architecture unsupported"; exit 1 ;;
 esac
 
+if ! command -v wget > /dev/null; then
+    # Wget is not available. Attempt to install it.
+    if command -v apt > /dev/null; then
+        apt-get update
+        apt-get install -yq wget
+    else
+        # Assume RPM based system
+        dnf install -yq wget
+    fi
+fi
+
 echo "Detected architecture: ${architecture}"
 
 wget --quiet -O /usr/local/bin/clusterctl "https://github.com/kubernetes-sigs/cluster-api/releases/download/${VERSION}/clusterctl-linux-${architecture}"
@@ -24,6 +35,7 @@ wget --quiet -O /usr/local/bin/clusterctl "https://github.com/kubernetes-sigs/cl
 chmod +x /usr/local/bin/clusterctl
 
 # Clusterctl bash completion
+mkdir -p /etc/bash_completion.d
 clusterctl completion bash > /etc/bash_completion.d/clusterctl.inc
 
 echo "Done!"
